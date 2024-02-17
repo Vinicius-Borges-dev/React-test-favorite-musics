0041fc9aae32d7977f69af983f95e4d4c2b6d942
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-02-17 17:33:03 -0300

    Atualização da estrutura do footer apenas por motivos estéticos.

--- src/Components/Common/Footer/index.jsx
+++ src/Components/Common/Footer/index.jsx
@@ -1,7 +1,9 @@
 function Footer() {
     return (
       <>
-        <h1>Footer</h1>
+        <footer className="p-5 text-center bg-dark mt-2 text-white">
+          <p className="fs-5">Todos os direitos reservados a Fav Musics &copy;</p>
+        </footer>
       </>
     );
   }
