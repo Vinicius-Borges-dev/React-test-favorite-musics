5512a31e54f618c0e3a1a86f1ffe200a71b3a321
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-02-17 18:00:32 -0300

    Configuração gerais dos arquivos e componentes, nada em especial por enquanto

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Components/Common/Footer";
+import Navbar from "../Components/Common/Navbar";
+
+function Home() {
+    return(
+        <>
+            <Navbar/>
+           <Footer/>
+        </>
+    )
+}
+
+export default Home;
