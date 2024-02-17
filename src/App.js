5d648860d17d8dc79150e28967bcc258d20e722a
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-02-17 17:31:46 -0300

    Troquei o conteudo do app.js para as rotas, para facilitar a identificação e a manutenção.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import AppRouter from './Router';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <AppRouter />
   );
 }
 
