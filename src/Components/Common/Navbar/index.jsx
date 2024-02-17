ddd602a2e38c31eb6652ffebd06dd2ad1b97c734
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-02-17 17:30:29 -0300

    Criado toda a estrutura do navbar com o uso de estilizações do framework bootstrap 5.2 (eu sei que as ultimas versões podem conter instabilidades, caso existam, eu os arrumo), foi utilizado também links de navegação via react

--- src/Components/Common/Navbar/index.jsx
+++ src/Components/Common/Navbar/index.jsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <>
-      <h1>Nav</h1>
+      <nav className="navbar bg-dark navbar-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to='/'>
+            <img
+              src="https://placehold.co/35"
+              alt=""
+              className="d-inline-block align-text-top"
+            />
+            Fav Musics
+          </Link>
+          <button type="button" className="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#MenuNavbar" aria-controls="MenuNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="MenuNavbar" aria-labelledby="MenuNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="MenuNavbarLabel">Menu</h5>
+        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/watch">Watch</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+        </div>
+      </nav>
     </>
   );
 }
