d6c226de424e0a07b567476ff8117d58603772ff
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-02-17 17:34:40 -0300

    Fazendo uso de componentes do react-router-dom para criar rotas e links.

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,18 @@
-function AppRouter(){
-    return (
-        
-    )
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Watch from "./Pages/Watch";
+import ErrorPage from "./Pages/ErrorPage";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
